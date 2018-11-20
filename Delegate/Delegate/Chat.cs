0af23599136b90a00d1ce5af0316a09efd9cfab4
1141986 <1141986@DIDATTICA.ROSSI.LOCAL>
2018-11-20 08:40:22 +0100

    Added new folder about delegate project in c#

---
+++ Delegate/Delegate/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Message
+    {
+        public Message() { }
+        public delegate void Stampa(string message);
+        public void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        /*Nell'esempio seguente viene dichiarato un delegato denominato 
+        [Del] che può incapsulare un metodo che accetta una stringa come argomento e restituisce void*/
+        public delegate void Del(string message);
+        public delegate void Somma(int n, int n1);
+        // Create a method for a delegate
+        //Del
+        public static void DelegateMethod(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+        //Somma
+        public static void DelegateSomma(int n, int n1)
+        {
+            System.Console.WriteLine(n + n1);
+        }
+        static void Main(string[] args)
+        {
+            // Instantiate the delegate.
+            Del handler = DelegateMethod;
+            Somma sum = DelegateSomma;
+            handler("Test");
+            sum(1, 2);
+            Message m = new Message();
+            m.Print("Message sent");
+            System.Console.ReadLine();
+        }
+        //Creo un metodo di tipo delegate;
+        //Creo un metodo di qualsiasi tipo che corrisponde a quello sopra di tipo delegate;
+        //Poi credo, come l'oggetto, un tipo delegate;
+    }
+}

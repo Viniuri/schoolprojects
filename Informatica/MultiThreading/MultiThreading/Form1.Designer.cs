248f92974fa64baddc3629f71cf4894bef510ad1
1141986 <1141986@DIDATTICA.ROSSI.LOCAL>
2018-11-30 09:44:03 +0100

    Modified multi threading file

---
+++ Informatica/MultiThreading/MultiThreading/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MultiThreading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.txtRisultato = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 113);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(94, 116);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(201, 20);
+            this.urlText.TabIndex = 1;
+            // 
+            // txtRisultato
+            // 
+            this.txtRisultato.Location = new System.Drawing.Point(94, 180);
+            this.txtRisultato.Multiline = true;
+            this.txtRisultato.Name = "txtRisultato";
+            this.txtRisultato.Size = new System.Drawing.Size(307, 85);
+            this.txtRisultato.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 410);
+            this.Controls.Add(this.txtRisultato);
+            this.Controls.Add(this.urlText);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.TextBox txtRisultato;
+    }
+}
+
---
+++ Informatica/MultiThreading/MultiThreading/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace MultiThreading
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            int i = await AccessTheWebAsync();
+            txtRisultato.Text = "Fine" + i.ToString();
+        }
+        private async Task <int> AccessTheWebAsync()
+        {
+            HttpClient client = new HttpClient();
+            string risultato = await client.GetStringAsync(urlText.Text);
+            DoWork();
+            return (risultato.Length);
+        }
+        private void DoWork()
+        {
+            int i = 0;
+            while (i++ < 10)
+            {
+                txtRisultato.Text += "working...";
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

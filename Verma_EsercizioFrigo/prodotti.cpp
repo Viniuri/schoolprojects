510b70bcc40862063ec85eb49ebded9aefa3e5ee
Jefe <itisrossi@gmail.com>
2018-12-03 21:24:45 +0100

    Made some big changes

--- Verma_EsercizioFrigo/prodotti.cpp
+++ Verma_EsercizioFrigo/prodotti.cpp
@@ -64,36 +64,75 @@ class Frigorifero {
     //Variabili private
     public:
         Prodotto prodotti[50];
+        bool occupato[50];
         Frigorifero(void) {
             for(int i = 0;i < 50; i++) {
-                prodotti[i] = Prodotto();
-            }
+                occupato[i] = false; //Quando vai a scrivere la funzione del prodotto che va ad occupato un posto
+            }						//Metti occupato come true ed aggiorni Prodotto prodotti[50]
         }
+        int occupaPosto(Prodotto prodotto){
+		    for (int i=0; i<50; i++) {
+		        if (occupato[i] == false) {
+		            prodotti[i] = prodotto;
+		            occupato[i] = true;
+		            return i;
+		        }
+		    }
+		    return -1; // nessun posto libero per barche a vela
+		}	
 };
 int main(int argc, char const *argv[])
 {
     Frigorifero frigorifero;
     Prodotto p, prodotti[50];
-    int scelta, cId, giorno, mese, anno, cal, nP;
+    int scelta, cId, giorno, mese, anno, cal, nP, posto;
     string nome, descr, tipo;
     do{
-    cout<<"Premi [1] per inserire un prodotto PRECOTTO\n";
-    cout<<"Premi [2] per inserire un prodotto NON-PRECOTTO\n";
-    cout<<"Scelta: ";cin>>scelta;
+    	cout<<"Premi [1] per inserire un prodotto PRECOTTO nel frigorifero\n";
+    	cout<<"Premi [2] per inserire un prodotto NON-PRECOTTO nel frigorifero\n";
+    	cout<<"Scelta ->> ";cin>>scelta;
     	switch(scelta)
     	{
-    		//string n, int cId, string descr, int giorniS, int meseS, int annoS, int cal, string tipo, int nP
-    		//string n, int cId, string descr, int giorniS, int meseS, int annoS, int cal
     		case 1:	
 				cout<<"Inserisci il nome del prodotto: ";cin>>nome;
 				cout<<"Inserisci il codice identificativo del prodotto: ";cin>>cId;
 				cout<<"Inserisci una breve descrizione del prodotto: ";cin>>descr;
-				cout<<"Inserisci la data di scadenza del prodotto, GIORNO: ";cin>>giorno;
-				cout<<"Inserisci la data di scadenza del prodotto, MESE: ";cin>>mese;
-				cout<<"Inserisci la data di scadenza del prodotto, ANNO: ";cin>>anno;
+				cout<<"Inserisci la data di scadenza del prodotto,\nGIORNO: ";cin>>giorno;
+				cout<<"MESE: ";cin>>mese;
+				cout<<"ANNO: ";cin>>anno;
 				cout<<"Inserisci le calorie del prodotto: ";cin>>cal;
 				cout<<"Inserisci la tipologia del prodotto: ";cin>>tipo;
 				cout<<"Inserisci il numero delle porzioni del prodotto: ";cin>>nP;
+				p = Prodotto(nome, cId, descr, giorno, mese, anno, cal, tipo, nP);
+				if ((posto = frigorifero.occupaPosto(p)) < 0)
+                {
+                	cout<<"Il frigorifero è pieno!\n";
+                }
+            	else
+                {
+                    cout<<"Prodotto inserito correttamente nel frigorifero\n";
+                    prodotti[posto] = p;
+                }
+				break;
+			case 2:	
+				cout<<"Inserisci il nome del prodotto: ";cin>>nome;
+				cout<<"Inserisci il codice identificativo del prodotto: ";cin>>cId;
+				cout<<"Inserisci una breve descrizione del prodotto: ";cin>>descr;
+				cout<<"Inserisci la data di scadenza del prodotto,\nGIORNO: ";cin>>giorno;
+				cout<<"MESE: ";cin>>mese;
+				cout<<"ANNO: ";cin>>anno;
+				cout<<"Inserisci le calorie del prodotto: ";cin>>cal;
+				p = Prodotto(nome, cId, descr, giorno, mese, anno, cal);
+				if ((posto = frigorifero.occupaPosto(p)) < 0)
+                {
+                	cout<<"Il frigorifero è pieno";
+                }
+            	else
+                {
+                    cout<<"Prodotto inserito correttamente nel frigo";
+                    prodotti[posto] = p;
+                }
+				break;		
 		}
 	}while(scelta!=0);
     return 0;

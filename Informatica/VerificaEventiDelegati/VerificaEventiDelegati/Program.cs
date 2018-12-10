aca86246e59bcf1e0912a4a762f21aa2862f2446
1141986 <1141986@DIDATTICA.ROSSI.LOCAL>
2018-12-10 08:25:34 +0100

    Added new sub-dir for VerificaEventiDelegati

---
+++ Informatica/VerificaEventiDelegati/VerificaEventiDelegati/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaEventiDelegati
+{
+    class Program
+    {
+        //Eventi
+        public void EventName(object sender, EventArgs e)
+        {
+
+        }
+        //
+        private delegate int Somma(int n1, int n2);
+        private static int DelegateSomma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        static void Main(string[] args)
+        {
+            int ris, n1, n2;
+            Somma somma = DelegateSomma;
+            Console.WriteLine("Digiti il primo numero");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digiti il secondo numero");
+            n2 = int.Parse(Console.ReadLine());
+            ris = somma(n1, n2);
+            Console.WriteLine(ris);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Informatica/VerificaEventiDelegati/VerificaEventiDelegati/myObj.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerificaEventiDelegati
+{
+    class myObj
+    {
+        //This delegate can be used to point to methods
+        //which return void and take a string.
+        public delegate void MyEventHandler(string foo);
+
+        //This event can cause any method which conforms
+        //to MyEventHandler to be called.
+        public static event MyEventHandler SomethingHappened;
+
+        //Here is some code I want to be executed
+        //when SomethingHappened fires.
+        static void HandleSomethingHappened(string foo)
+        {
+            //Do something
+        }
+
+        private static void Main(object sender, EventArgs args)
+        {
+            //I am creating a delegate (pointer) to HandleSomethingHappened
+            //and adding it to SomethingHappened's list of "Event Handlers".
+            myObj.SomethingHappened += new MyEventHandler(HandleSomethingHappened);
+
+            //To raise the event within a method.
+            SomethingHappened("bar");
+        }
+    }
+}

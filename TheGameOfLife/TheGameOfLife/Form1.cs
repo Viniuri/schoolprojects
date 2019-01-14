41861d0cebd2fb39f302077bf9880004c5e3f1f1
1141986 <1141986@DIDATTICA.ROSSI.LOCAL>
2019-01-14 08:40:11 +0100

    game of life

---
+++ TheGameOfLife/TheGameOfLife/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TheGameOfLife
+{
+    public partial class Form1 : Form
+    {
+        private static readonly Random rand = new Random();
+        CEsserevivente[,] CEv = new CEsserevivente[10,10]; 
+        public Form1()
+        {
+            InitializeComponent();
+            randomSpawn();
+        }
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        public void randomSpawn()
+        {
+            PictureBox pCarrot;
+            for (int i = 0; i < 20; i++)
+            {
+                pCarrot = new PictureBox();
+                pCarrot.Image = Image.FromFile("C:/TGOL/carrot.png");
+                pCarrot.Size = new Size(table.Width / 10, table.Height / 10);
+                pCarrot.SizeMode = PictureBoxSizeMode.StretchImage;
+                int r = GetRandomNumber(0, 10);
+                int c = GetRandomNumber(0, 10);
+                table.Controls.Add(pCarrot, c, r);
+                this.CEv[r, c] = new CCarota(r, c); 
+            }
+            PictureBox pRabbit;
+            for (int i = 0; i < 14; i++)
+            {
+                pRabbit = new PictureBox();
+                pRabbit.Image = Image.FromFile("C:/TGOL/rabbit.png");
+                pRabbit.Size = new Size(table.Width / 10, table.Height / 10);
+                pRabbit.SizeMode = PictureBoxSizeMode.StretchImage;
+                int r = GetRandomNumber(0, 10);
+                int c = GetRandomNumber(0, 10);
+                table.Controls.Add(pRabbit, c, r);
+                this.CEv[r, c] = new CConiglio(r, c);
+            }
+            PictureBox pFox;
+            for (int i = 0; i < 8; i++)
+            {
+                pFox = new PictureBox();
+                pFox.Image = Image.FromFile("C:/TGOL/fox.png");
+                pFox.Size = new Size(table.Width / 10, table.Height / 10);
+                pFox.SizeMode = PictureBoxSizeMode.StretchImage;
+                int r = GetRandomNumber(0, 10);
+                int c = GetRandomNumber(0, 10);
+                table.Controls.Add(pFox, c, r);
+                this.CEv[r, c] = new CVolpe(r, c);
+            }
+        }
+        //Different random number from previous
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (rand)
+            {
+                return rand.Next(min, max);
+            }
+        }
+        private void button_Click(object sender, EventArgs e)
+        {
+            for (int rRand = 0; rRand < 10; rRand++)
+            {
+                for (int cRand = 0; cRand < 10; cRand++)
+                {
+                    
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Controlla evento click
+            foreach (PictureBox space in this.table.Controls)
+            {
+                space.MouseClick += new MouseEventHandler(clickOnSpace);
+            }
+        }
+        public void clickOnSpace(object sender, MouseEventArgs e)
+        {
+            int CimgSelected = table.GetColumn((PictureBox)sender);
+            int RimgSelected = table.GetRow((PictureBox)sender);
+            Where(RimgSelected, CimgSelected);
+            //MessageBox.Show(RimgSelected.ToString() + "," + CimgSelected.ToString());
+        }
+        private void Where(int r, int c)
+        {
+            Control img = table.GetControlFromPosition(c, r);
+            int r1 = GetRandomNumber(-2, 2);
+            int c1 = GetRandomNumber(-2, 2);
+            r += r1;
+            c += c1;
+            if(r<0)
+            {
+                r = 0;
+            }
+            if(r>10)
+            {
+                r = 10;
+            }
+            if(c<0)
+            {
+                c = 0;
+            }
+            if(c>10)
+            {
+                c = 10;
+            }
+            table.Controls.Add(img, c, r);
+        }
+    }
+}
